8dfa373494f7689cbec3e216fa00562c7cbbd149
Yong Lim <yonglim@Yongs-Mac-mini.domain.name>
2023-01-23 11:36:55 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Original source
+
+Please go to this site for the original source tree:  [Business Jekyll Theme](https://business-jekyll-theme.github.io)
+
---
+++ js/jordan.js
@@ -0,0 +1,66 @@
+$(function() {
+  $('.form-control').focus(formFocus);
+});
+
+function formFocus() {
+  $('#alert-field')
+    .removeClass()
+    .addClass('hidden');
+}
+
+function sendElephant(e) {
+  e.preventDefault();
+
+  console.log("in sendElephant");
+  
+  // const POST_URL = 'https://script.google.com/macros/s/AKfycbwQ_wdqOBA_Z-29b9s2BZ7GHB3bGhlVzRL2hNwKdp-KoTQ5vhQ5bxz8uinBuKwvKDzP/exec'
+  // const postRequest = {
+  //   name: e.target['name-field'].value,
+  //   phone: e.target['phone-field'].value,
+  //   email: e.target['email-field'].value,
+  //   body: e.target['body-field'].value
+  // };
+  
+  const POST_URL_DEV = 'https://script.google.com/macros/s/AKfycbzXWAaJ91Cii0jK2A8AtEZNMxRYnv7n7TrpoLBihV8j/dev'
+  const POST_URL = 'https://script.google.com/macros/s/AKfycbw8LIqY1yATgL84jVImRa1jg5WtbanUW1ksGZc7G8BJxZczi6jjlky89Vz7DYIfm0M_pA/exec'
+  const postRequest = {
+    name: e.target['fullname'].value,
+    email: e.target['email'].value,
+    message: e.target['message'].value,
+  };
+
+  if(POST_URL) {
+    $.post(POST_URL, JSON.stringify(postRequest))
+      .then(res => {
+        e.target.reset();
+        $('#alert-field')
+          .removeClass()
+          .addClass(`alert alert-${res.code}`)
+          .text(res.msg);
+      });
+
+    $('#alert-field')
+      .removeClass()
+      .html('<progress></progress>')
+      .removeClass('hidden');
+  } else {
+    alert('You must set the POST_URL variable with your script ID');
+  }
+}
+
+function changeSubject(e) {
+  if(e.target.value === 'Other') {
+    $('#subject-select').removeClass('col-xs-12')
+      .addClass('col-xs-6');
+    $('#hidden-other-subject').removeClass('hidden');
+  } else {
+    $('#subject-select').removeClass('col-xs-6')
+      .addClass('col-xs-12');
+
+    $('#hidden-other-subject').addClass('hidden');
+  }
+}
+
+
+
+
